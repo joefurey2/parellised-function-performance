507cd8a2b77f6f4ea0b22e649b05bc2531b6e521
joefurey2 <joefurey02@gmail.com>
2024-03-15 11:33:40 +0000

    updated go code formatting

--- open-faas/threaded-matricies/threaded-matrix/handler.go
+++ open-faas/threaded-matricies/threaded-matrix/handler.go
@@ -12,57 +12,57 @@ import (
 )
 
 func multiply(a, b [][]int, result [][]int, start, end int) {
-    for i := start; i < end; i++ {
-        for j := 0; j < len(b[0]); j++ {
-            for k := 0; k < len(b); k++ {
-                result[i][j] += a[i][k] * b[k][j]
-            }
-        }
-    }
+	for i := start; i < end; i++ {
+		for j := 0; j < len(b[0]); j++ {
+			for k := 0; k < len(b); k++ {
+				result[i][j] += a[i][k] * b[k][j]
+			}
+		}
+	}
 }
 
 func matrixMultiplication(rows, cols, numProcesses int) [][]int {
-    a := make([][]int, rows)
-    for i := range a {
-        a[i] = make([]int, cols)
-        for j := range a[i] {
-            a[i][j] = rand.Intn(91) + 10
-        }
-    }
+	a := make([][]int, rows)
+	for i := range a {
+		a[i] = make([]int, cols)
+		for j := range a[i] {
+			a[i][j] = rand.Intn(91) + 10
+		}
+	}
 
-    b := make([][]int, rows)
-    for i := range b {
-        b[i] = make([]int, cols)
-        for j := range b[i] {
-            b[i][j] = rand.Intn(91) + 10
-        }
-    }
+	b := make([][]int, rows)
+	for i := range b {
+		b[i] = make([]int, cols)
+		for j := range b[i] {
+			b[i][j] = rand.Intn(91) + 10
+		}
+	}
 
-    // Initialize result matrix
-    result := make([][]int, rows)
-    for i := range result {
-        result[i] = make([]int, cols)
-    }
+	// Initialize result matrix
+	result := make([][]int, rows)
+	for i := range result {
+		result[i] = make([]int, cols)
+	}
 
-    var wg sync.WaitGroup
-    wg.Add(numProcesses)
+	var wg sync.WaitGroup
+	wg.Add(numProcesses)
 
-    // Perform matrix multiplication
-    for i := 0; i < numProcesses; i++ {
-        go func(i int) {
-            defer wg.Done()
-            start := (rows / numProcesses) * i
-            end := start + (rows / numProcesses)
-            if i == numProcesses-1 {
-                end = rows
-            }
-            multiply(a, b, result, start, end)
-        }(i)
-    }
+	// Perform matrix multiplication
+	for i := 0; i < numProcesses; i++ {
+		go func(i int) {
+			defer wg.Done()
+			start := (rows / numProcesses) * i
+			end := start + (rows / numProcesses)
+			if i == numProcesses-1 {
+				end = rows
+			}
+			multiply(a, b, result, start, end)
+		}(i)
+	}
 
-    wg.Wait()
+	wg.Wait()
 
-    return result
+	return result
 }
 
 func Handle(w http.ResponseWriter, r *http.Request) {
@@ -92,10 +92,10 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		elapsedSec := fmt.Sprintf("%.8f", elapsed.Seconds())
 
 		response := map[string]interface{}{
-			"result":   result,
+			"result":      result,
 			"matrix size": rows * cols,
-			"execTime": elapsedSec,
-			"numProcs": numProcs,
+			"execTime":    elapsedSec,
+			"numProcs":    numProcs,
 		}
 
 		responseJSON, err := json.Marshal(response)
@@ -106,7 +106,7 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(responseJSON)
-		
+
 	} else {
 		message := "Error with times value passed"
 		http.Error(w, message, http.StatusBadRequest)
